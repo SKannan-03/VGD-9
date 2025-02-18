08a510d4c0b9af3e5ccd642f631d317a3a47ac0a
SKannan-03 <sarveshkannan03@gmail.com>
2025-02-18 12:19:27 -0700

    Create Feb 18, 2025.cs

---
+++ Feb 18, 2025.cs
@@ -0,0 +1,10 @@
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temeprature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
