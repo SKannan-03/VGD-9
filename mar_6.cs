a40c514eacb769f8b07ea606e41f43fde6d5a31a
SKannan-03 <sarveshkannan03@gmail.com>
2025-03-06 10:23:53 -0700

    Update mar_6.cs

--- mar_6.cs
+++ mar_6.cs
@@ -1,6 +1,16 @@
 //We learnt aout literal values and using single quotations. 
+//happy birthday Ghana!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 Console.WriteLine("123");
 Console.WriteLine(123);
 
 Console.WriteLine("true");
 Console.WriteLine(true);
+
+
+
+//notes
+Float Type    Precision
+----------------------------
+float         ~6-9 digits
+double        ~15-17 digits
+decimal        28-29 digits
