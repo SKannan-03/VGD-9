fc87e8ad01f6433f0e0e82c502962dc76142744f
SKannan-03 <sarveshkannan03@gmail.com>
2025-04-10 10:07:05 -0600

    Create Apr_10.cs

---
+++ Apr_10.cs
@@ -0,0 +1,5 @@
+// Today we learnt about how to add integers together and alos including strings within the output. This allows me to use both stings and integers together to create mixed outputs.
+
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
